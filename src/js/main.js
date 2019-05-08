8467d34afc68e2b7e275ae3d0679b75e1e9de7a7
dzoniD <edvard_dantes@hotmail.com>
2019-05-08 13:11:29 +0200

    updated database and refactor js for adding player cards

--- src/js/main.js
+++ src/js/main.js
@@ -8,44 +8,34 @@ async function getBase(location) {
   return responsFromBase.data;
 };
 
-async function renderAds() {
-  const listings = await getBase('/players?_sort=id&_order=desc');
+async function renderCards() {
+  const listings = await getBase('/players?_sort=id');
   const limitListings = listings.slice(0, 8);
-  (async () => await _render_small(limitListings, '.playersCards'))();
-  $('body').removeAttr('onload');
+  (async () => await _renderPlayerCard(listings, '.playersCards'))();
 };
 
-async function _render_small(listings, location) {
-  for (const player of players) {
-    const users = await getBase(`/players/${players.id}`);
-    const $adsContainer = $(`${location}`);
-    const $ad = $(`<div id="singlePlayerCard">
-      <img class="Hea" src="${ad.imgUrl}" alt="">
+async function _renderPlayerCard(listings, location) {
+  for (const player of listings) {
+    const users = await getBase(`/players/${player.id}`);
+    const $playersCards = $(`${location}`);
+    const $player = $(
+      `<div id="singlePlayerCard">
+      <img class="Hea" src="${player.imgUrl}" alt="">
 
       <div class="singleCardText">
-          <h3>${ad.jerseyNo}</h3>
-          <p>${ad.name} <br>${ad.lastName}</p>
-
+          <h3>${player.jerseyNo}</h3>
+          <p>${player.name} <br>${player.lastName}</p>
       </div>
-  </div>`);
-    $ad.appendTo($adsContainer);
-    // $(`.image_${ad.id}`).on('click', () => fullAds(ad.id));
-    // $(`#fav_${ad.id}`).on('click', addToFavorites);
+    </div>`);
+    $player.appendTo($playersCards);
   }
 };
 
 function onLoadHTML() {
   const page = location.href;
   if (page.search('/players.html') >= 0) {
-      return renderAds();
+    return renderCards();
   }
-  //  else if (page.search('/ad.html') >= 0) {
-  //     return renderFullAds();
-  // } else if (page.search('/publish_ad.html') >= 0) {
-  //     return loadAdToForm();
-  // } else if (page.search('/user_panel.html') >= 0) {
-  //     return usersAds();
-  // }
 };
 
 $(document).on('load', onLoadHTML());
